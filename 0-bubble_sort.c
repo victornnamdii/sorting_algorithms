bedf012ba0f5a06bf3dced5295160e1a2e1cb475
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-12 07:16:40 -0700

    Edit: 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,9 +14,9 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for ( ; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for ( ; j < (size - 1); j++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
