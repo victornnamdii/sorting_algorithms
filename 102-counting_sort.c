23fa5999e74182858e06d2217ab81c7060032ad5
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-15 05:59:08 -0700

    Add: 102

---
+++ 102-counting_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+ * count_array - creates the count array
+ * @array: array of integers
+ * @size: size of array
+ * @k: largest number in unsorted array
+ * Return: the count array
+ */
+int *count_array(int *array, size_t size, int k)
+{
+	int *ca, i;
+	size_t j;
+
+	ca = malloc((k + 1) * sizeof(int));
+	for (i = 0; i < (k + 1); i++)
+		ca[i] = 0;
+	for (j = 0; j < size; j++)
+		ca[array[j]]++;
+	for (i = 1; i <= k; i++)
+		ca[i] = ca[i] + ca[i - 1];
+	print_array(ca, k + 1);
+	return (ca);
+}
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *b, k, *ca, j;
+	size_t i;
+
+	if (array == NULL || size < 2)
+		return;
+	k = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > k)
+			k = array[i];
+	}
+	ca = count_array(array, size, k);
+	b = malloc(size * sizeof(int));
+
+	for (j = size - 1; j >= 0; j--)
+		b[--ca[array[j]]] = array[j];
+	for (i = 0; i < size; i++)
+		array[i] = b[i];
+	free(b);
+	free(ca);
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef __SORT_H__
 #define __SORT_H__
 
+#include <stdio.h>
 #include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
@@ -24,5 +25,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
