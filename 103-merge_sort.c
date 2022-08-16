4a2440d0734423f44710369fa779552009e898a0
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-16 04:02:27 -0700

    Edit(betty): 103

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -15,30 +15,30 @@
  */
 void TDMerge(size_t start, size_t middle, size_t end, int *dest, int *source)
 {
-	  size_t i, j, k;
+	size_t i, j, k;
 
- 	  printf("Merging...\n");
-	  printf("[left]: ");
-	  print_array(source + start, middle - start);
-	  printf("[right]: ");
-	  print_array(source + middle, end - middle);
-	  i = start;
-	  j = middle;
-	  for (k = start; k < end; k++)
-	  {
-		  if (i < middle && (j >= end || source[i] <= source[j]))
-		  {
-			  dest[k] = source[i];
-			  i++;
-		  }
-		  else
-		  {
-			  dest[k] = source[j];
-			  j++;
-		  }
-	  }
-	  printf("[Done]: ");
-	  print_array(dest + start, end - start);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(source + start, middle - start);
+	printf("[right]: ");
+	print_array(source + middle, end - middle);
+	i = start;
+	j = middle;
+	for (k = start; k < end; k++)
+	{
+		if (i < middle && (j >= end || source[i] <= source[j]))
+		{
+			dest[k] = source[i];
+			i++;
+		}
+		else
+		{
+			dest[k] = source[j];
+			j++;
+		}
+	}
+	printf("[Done]: ");
+	print_array(dest + start, end - start);
 }
 
 /**
@@ -52,16 +52,16 @@ void TDMerge(size_t start, size_t middle, size_t end, int *dest, int *source)
  */
 void TDSplitMerge(size_t start, size_t end, int *array, int *copy)
 {
-	  size_t middle;
+	size_t middle;
 
-	  if (end - start < 2)
-		  return;
-	  middle = (start + end) / 2;
-	  TDSplitMerge(start, middle, array, copy);
-	  TDSplitMerge(middle, end, array, copy);
-	  TDMerge(start, middle, end, array, copy);
-	  for (middle = start; middle < end; middle++)
-		  copy[middle] = array[middle];
+	if (end - start < 2)
+		return;
+	middle = (start + end) / 2;
+	TDSplitMerge(start, middle, array, copy);
+	TDSplitMerge(middle, end, array, copy);
+	TDMerge(start, middle, end, array, copy);
+	for (middle = start; middle < end; middle++)
+		copy[middle] = array[middle];
 }
 
 /**
@@ -74,16 +74,16 @@ void TDSplitMerge(size_t start, size_t end, int *array, int *copy)
  */
 void merge_sort(int *array, size_t size)
 {
-	  size_t i;
-	  int *copy;
+	size_t i;
+	int *copy;
 
-	  if (array == NULL || size < 2)
-		  return;
-	  copy = malloc(sizeof(int) * size);
-	  if (copy == NULL)
-		  return;
-	  for (i = 0; i < size; i++)
-		  copy[i] = array[i];
-	  TDSplitMerge(0, size, array, copy);
-	  free(copy);
+	if (array == NULL || size < 2)
+		return;
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		copy[i] = array[i];
+	TDSplitMerge(0, size, array, copy);
+	free(copy);
 }
